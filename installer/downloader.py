7ec4f29944f790879d21c1afe73c33e7b54b38bd
Heewoon Kim <heewoonkim2020@gmail.com>
2022-06-08 16:55:37 +0900

    Installer download & GitIgnore

--- installer/downloader.py
+++ installer/downloader.py
@@ -7,7 +7,7 @@ install_bool = True if install_user.lower() == "y" else False
 
 if install_bool:
     print("[INFO] Tip: The installation won't take longer than 20 seconds, trust me!")
-    download.download(url="")
+    download.download(path="EasyUtil_Windows/", url="https://github.com/heewoonkim2020/EasyUtil/archive/refs/heads/main.zip")
 else:
     print("[INFO] Ok, cancelling installation")
     print("[INFO] Thank You And Goodbye")
